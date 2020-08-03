5da9da5eabdfc840c18a72faecb281933b931672
Juan Abulencia <juan.abulencia@gmail.com>
2020-08-03 15:16:14 -0700

    Modify Infra - add security group and user data

--- main.tf
+++ main.tf
@@ -2,11 +2,29 @@ provider "aws" {
     region = "us-west-1"
 }
 
-resource "aws_instance" "Juan-TF-WL1" {
+resource "aws_instance" "Ysa-Workloads" {
     ami = "ami-05655c267c89566dd"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+               #!/bin/bash
+               echo "Hello, World" > index.html
+               nohup busybox httpd -f -p 8080 &
+               EOF
 
     tags = {
         Name = "Ysa-Test-WL1"
+    }
+}
+
+resource "aws_security_group" "instance" {
+    name = "Ysa-WebApp-SecurityGroup"
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
